159ebc1e779d71776cbe3da735c3de3b6dd31799
giri-10 <90170719+giri-10@users.noreply.github.com>
2023-12-15 06:56:49 +0530

    feat: Completed implementation

---
+++ keywords.py
@@ -0,0 +1,12 @@
+medical_keywords = set([
+    "symptom", "disease", "treatment", "medicine", "diagnosis", "doctor", "hospital", 
+    "surgery", "health", "illness", "prescription", "therapy", "infection", "virus", 
+    "bacteria", "inflammation", "pain", "fever", "allergy", "cancer", "diabetes", "heart", 
+    "lung", "brain", "skin", "eye", "ear", "nose", "throat", "blood", "cell", "gene", 
+    "vaccine", "immunity", "nutrition", "exercise", "mental", "stress", "anxiety", 
+    "depression", "injury", "fracture", "scan", "test", "x-ray", "mri", "ultrasound", 
+    "ct scan", "prescription", "medication", "side effects", "prognosis", "recovery", 
+    "rehabilitation", "physiotherapy", "diet", "weight loss", "obesity", "pregnancy", 
+    "birth", "baby", "child", "pediatric", "geriatric", "elderly", "senior", "chronic", 
+    "acute", "emergency", "urgent", "appointment", "consultation", "referral", "second opinion"
+])
--- telegram_bot.py
+++
@@ -1,6 +0,0 @@
-from typing import Final
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypesfffffsf
-
-TOKEN: Final = '6521571281:AAGTSRjrI9nG8x6r-wFtVx1ut8LY_oPYykU'
-BOT_USERNAME: Final = '@meddddbot'
